65d0caa4f1ce5eb093e1b1b104962555d8b2f9a3
[200~jonhuilson008 <jonhuilson008@gmail.com>
2023-03-29 22:45:12 +0500

    kun.uz

--- src/main/java/com/example/KunUz1Application.java
+++ src/main/java/com/example/KunUz1Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class KunUz1Application {
 
+
 	public static void main(String[] args) {
 //		int a = 2 ^ 3 + 8;
 //		System.out.println(a);
